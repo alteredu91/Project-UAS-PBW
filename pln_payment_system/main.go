b83a603e4a69136b4c8e41a3ae7fb677f1344537
alteredu91 <79585685+alteredu91@users.noreply.github.com>
2024-07-08 08:52:52 +0700

    Add files via upload

---
+++ pln_payment_system/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "database/sql"
+    "log"
+    "net/http"
+
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/gorilla/mux"
+    "github.com/gorilla/sessions"
+    "pln_payment_system/controllers"
+)
+
+var store = sessions.NewCookieStore([]byte("secret-key"))
+
+func main() {
+    router := mux.NewRouter()
+
+    // Database connection
+    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/pln_payment")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Controllers
+    userController := controllers.NewUserController(db, store)
+    adminController := controllers.NewAdminController(db, store)
+    paymentController := controllers.NewPaymentController(db, store)
+
+    router.HandleFunc("/register", userController.Register).Methods("POST")
+    router.HandleFunc("/login", userController.Login).Methods("POST")
+    router.HandleFunc("/logout", userController.Logout).Methods("POST")
+    router.HandleFunc("/user-info", userController.UserInfo).Methods("GET")
+    router.HandleFunc("/admin/login", userController.AdminLogin).Methods("POST")
+    router.HandleFunc("/admin/user-info", adminController.AdminUserInfo).Methods("GET")
+    router.HandleFunc("/admin/users", adminController.IndexUsers).Methods("GET")
+    router.HandleFunc("/admin/payments", adminController.IndexPayments).Methods("GET")
+
+    router.HandleFunc("/payments", paymentController.Index).Methods("GET")
+    router.HandleFunc("/payments", paymentController.Create).Methods("POST")
+    router.HandleFunc("/payments/{id}", paymentController.Update).Methods("PUT")
+    router.HandleFunc("/payments/{id}", paymentController.Delete).Methods("DELETE")
+
+    // Static files
+    router.PathPrefix("/").Handler(http.FileServer(http.Dir("./views/")))
+
+    log.Println("Server started on :8080")
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
---
+++ pln_payment_system/models/payment.go
@@ -0,0 +1,9 @@
+package models
+
+type Payment struct {
+    ID            int     `json:"id"`
+    UserID        int     `json:"user_id"`
+    Amount        float64 `json:"amount"`
+    Method        string  `json:"method"`
+    TransactionID string  `json:"transaction_id"`
+}
